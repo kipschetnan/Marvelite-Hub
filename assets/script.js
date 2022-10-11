0b53b25147e65207b7a5746227e06b2a85f57504
Minh Truong <mptruong49@gmail.com>
2022-10-10 18:35:41 -0600

    character fetch button

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,28 @@
-var APIkey = 'https://gateway.marvel.com:443/v1/public/characters?name=thor&apikey=447061714da1f776acf1c2d309091175'
-var searchBtn = document.getElementById('search-btn');
+// Global variables
+const APIkey = '447061714da1f776acf1c2d309091175'
+const characterInput = document.getElementById('character-input');
+const searchBtn = document.getElementById('search-btn');
 
+// fetch character and subdomain
 function fetchMarvel(event) {
     event.preventDefault();
-    fetch(APIkey)
+    
+    let heroName = $('#character-input').val();
+    
+    let characterAPIkey = `https://gateway.marvel.com:443/v1/public/characters?name=${heroName}&apikey=${APIkey}`
+    fetch(characterAPIkey)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
         console.log(data);
+        if (data.code !== 200) {
+          return
+        }
+        
       });
 }
+
+
 // fetchMarvel()
 searchBtn.addEventListener('click', fetchMarvel);     
