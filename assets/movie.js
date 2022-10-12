1cee9ee49b21b387b30bbd2550fc5eadd1f378d4
BigSev15 <palat008@umn.edu>
2022-10-11 20:18:02 -0500

    working on appending movie info to the page

--- assets/movie.js
+++ assets/movie.js
@@ -1,6 +1,6 @@
 // Global variables
 const APIKey2 = '5cf37e04';
-
+const movieDisplay = $('#');
 //fetch movie info
 function fetchOMDB() {
 
@@ -11,6 +11,19 @@ function fetchOMDB() {
       })
       .then(function(data) {
         console.log(data);
+        if (!data.Response) {
+          return;
+        }
+      
+        var results = data.Search;
+        for (var i=0; i < results.length; i++) {
+          var title = results[i].Title;
+          var year = results[i].Year;
+          var posterPath = results[i].Poster;
+          var poster = $(`<img src=${posterPath}>`);
+          var movieTitle = $('<p class = "movie">' + title + '</p>');
+          var movieYear = $('<p class="year">' + year + '</p>');
+        }
       });
 }
 
