4c3ad5a5c46d50dda58308844d8b60fe1edcbbc8
Minh Truong <mptruong49@gmail.com>
2022-10-12 23:05:52 -0600

    save search to local, clear btn also clear local storage, PRETTIFIER

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,32 @@
 // Global variables
-const APIkey = '447061714da1f776acf1c2d309091175'
-const characterInput = document.getElementById('character-input');
-const searchBtn = document.getElementById('search-btn');
-const redirectBtn = document.getElementById('redirect');
-let history = JSON.parse(localStorage.getItem('history')) || [];
-const displayChar = $('#character-display');
-const movieBtnEl = document.getElementById('movieBtn');
-const clearHistBtn = document.getElementById('clearHistoryBtn');
-const clearHistDiv = document.getElementById('clear-history');
-
+const APIkey = "447061714da1f776acf1c2d309091175";
+const characterInput = document.getElementById("character-input");
+const searchBtn = document.getElementById("search-btn");
+const redirectBtn = document.getElementById("redirect");
+let history = JSON.parse(localStorage.getItem("heroArr")) || [];
+const displayChar = $("#character-display");
+const movieBtnEl = document.getElementById("movieBtn");
+const clearHistBtn = document.getElementById("clearHistoryBtn");
+const clearHistDiv = document.getElementById("clear-history");
 
 function fetchComics(ID) {
   let comicId = ID;
-  let getComicURL = `https://gateway.marvel.com:443/v1/public/comics/${comicId}?apikey=447061714da1f776acf1c2d309091175`
+  let getComicURL = `https://gateway.marvel.com:443/v1/public/comics/${comicId}?apikey=447061714da1f776acf1c2d309091175`;
 
   fetch(getComicURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (comicData) {
-      if(comicData.code !== 200) {
+      if (comicData.code !== 200) {
         return;
       }
       console.log(comicData);
-
-    })
+    });
 }
 // fetch character and subdomain
 function fetchMarvel(heroName) {
-  
-  //let heroName = $('#character-input').val();
-  
-  let getCharURL = `https://gateway.marvel.com:443/v1/public/characters?name=${heroName}&apikey=${APIkey}`
+  let getCharURL = `https://gateway.marvel.com:443/v1/public/characters?name=${heroName}&apikey=${APIkey}`;
   fetch(getCharURL)
     .then(function (response) {
       return response.json();
@@ -42,19 +37,19 @@ function fetchMarvel(heroName) {
       }
       console.log(characterData);
       // check if user's input can be save in local storage
-      if (characterData.data.results.length !== 0){
+      if (characterData.data.results.length !== 0) {
         //looks to see if the hero is in the array
-        findHero = history.find( e => e.description == heroName);
+        findHero = history.find((e) => e.description == heroName);
         console.log(findHero);
         //if the hero is already on the array then returns true otherwise false
         heroExists = findHero ? true : false;
         console.log(heroExists);
-
-        if(!heroExists) {
-          history.push( { description: heroName })
+        // If heroName is not existed in history array, save to local and render search list
+        if (!heroExists) {
+          history.push({ description: heroName });
+          localStorage.setItem("heroArr", JSON.stringify(history));
+          buildHistory();
         }
-        buildHistory()
-        
       }
 
       // append character's name, thumbnail, and description
@@ -63,10 +58,12 @@ function fetchMarvel(heroName) {
       let charDescription = results.description;
       let imagePath = results.thumbnail.path;
       let imageExtension = results.thumbnail.extension;
-      let imageSrc = `${imagePath}/portrait_xlarge.${imageExtension}`
-      let characterName = $('<p class="character-name">' + character + '</p>');
+      let imageSrc = `${imagePath}/portrait_xlarge.${imageExtension}`;
+      let characterName = $('<p class="character-name">' + character + "</p>");
       let thumbnail = $(`<img src=${imageSrc}>`);
-      let description = $('<p class="character-desc">' + charDescription + '</p>');
+      let description = $(
+        '<p class="character-desc">' + charDescription + "</p>"
+      );
       // Append character's name, thumbnail and description on the page
       displayChar.append(characterName);
       displayChar.append(thumbnail);
@@ -76,42 +73,41 @@ function fetchMarvel(heroName) {
         // get comic IDs
         let comics = results.comics.items;
         let resourceURI = comics[i].resourceURI;
-        let splitArray = resourceURI.split('/');
+        let splitArray = resourceURI.split("/");
         let comicID = splitArray[6];
         // display a list of comics
         fetchComics(comicID);
-        
       }
       displayMovieRedirectButton();
       displayClearHistoryButton();
-      
     });
 }
 //Builds the history block
 function buildHistory() {
-  $(".search-history").empty()
-  history.forEach(h => {
-    let li = $("<li>").text(h.description)
+  $(".search-history").empty();
+  history.forEach((h) => {
+    let li = $("<li>").text(h.description);
     //when clicking on one of the characters in the history it searches that character
     li.click(function (event) {
       event.preventDefault();
       fetchMarvel(event.target.textContent);
-    })
-    $(".search-history").append(li)
-  })
+    });
+    $(".search-history").append(li);
+  });
 }
 //displays the clear history button and clears the history array
-function displayClearHistoryButton () {
-  clearHistDiv.style.display = 'block';
+function displayClearHistoryButton() {
+  clearHistDiv.style.display = "block";
   clearHistBtn.onclick = function () {
     history = [];
-    buildHistory()
-  }
+    localStorage.clear();
+    buildHistory();
+  };
 }
 //displays redirect btn
 function displayMovieRedirectButton() {
   //displays the button
-  redirectBtn.style.display = 'block';
+  redirectBtn.style.display = "block";
 
   //redirects to the other html page
   movieBtnEl.onclick = function () {
@@ -121,9 +117,10 @@ function displayMovieRedirectButton() {
 
 function searchBtnHandler(event) {
   event.preventDefault();
-  let heroName = $('#character-input').val();
-  fetchMarvel(heroName)
+  let heroName = $("#character-input").val();
+  fetchMarvel(heroName);
 }
+// Invoke on load, render search history
+buildHistory();
 
-searchBtn.addEventListener('click', searchBtnHandler);    
-
+searchBtn.addEventListener("click", searchBtnHandler);
